1f85df0d1229657ae981e8f39c1938d4dac71739
Andres Martinez Martinez <andresmtzmtz@MacBook-Air-de-Andres.local>
2019-03-02 00:21:50 -0600

    Ya agregue mi parte

--- Snake.py
+++ Snake.py
@@ -20,3 +20,45 @@ food=[sh/2,sw/2]
 w.addch(food[0],food[1],curses.ACS_PI)
 
 key=curses.KEY_RIGHT
+#Definimos la direccion inicial de la serpiente, ya sea arriba, abajo, izquierda o derecha
+
+
+while True:
+#El juego se desarrolla dentro de este while
+
+    next_key = w.getch()
+#w.getch() espera un caracter, cuando sea introducido se guardara en next_key
+
+    key = key if next_key == -1 else next_key
+    #La llave va a permanecer igual si no se introduce nada, si si se introduce entonces tomara el valor de lo que se introdujo
+
+    if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
+        #Si la serpiente choca en el limite de arriba o de abajo; o si choca con el de la derecha o izquierda
+        #o si la serpiente se encuentra con si misma
+
+        curses.endwin()
+        quit()
+        #Entonces se acaba el juego y se regresa a la terminal
+
+    new_head = [snake[0][0], snake[0][1]]
+    #la direccion de la serpiente primero se define como su posicion inicial/actual
+
+    if key == curses.KEY_DOWN:
+        #Si se presiona la tecla flecha de Abajo 
+
+        new_head[0] += 1
+        #Entonces la "posicion Y" de la serpiente se le sumara 1, indicando que va hacia arriba
+
+    if key == curses.KEY_UP:
+        #Si se presiona la tecla flecha de Arriba
+
+        new_head[0] -= 1
+        #Entonces la "posicion Y" de la serpiente se le resta 1, indicando que va hacia abajo
+
+    if key == curses.KEY_LEFT:
+        #Si se presiona la tecla flecha de la Izquierda
+
+        new_head[1] -= 1
+        #Entonces la "posicion X" de la serpiente se le restara 1, indicando que va hacia a la izquierda
+
+
