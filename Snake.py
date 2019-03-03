0448bc22cfdf9a2bc1af9806a4da6bb0d2ddb491
Sofía Colín <sofiacolinscl@gmail.com>
2019-03-02 20:48:51 -0600

    Revise el código, comente algunas cosas que no me habían quedado muy claras y corregí algunos errores, amigos tengan cuidado con la escritura de los identificadores

--- Snake.py
+++ Snake.py
@@ -1,33 +1,49 @@
 import random
-import curses ##Se importa un modulo de Python para crear una interfaz gráfica.
 
-s=curses.initscr() ##Se inciializa la aplicación de curses.
+#Se importa un modulo de Python para crear una interfaz gráfica y manejar el teclado desde la consola
+import curses 
+
+'''
+Las siguientes lineas crearán una ventana donde se desarrollara el juego, para 
+lo cual se posiciona el cursor en 0 y por medio de la función getmaxyx se obtiene 
+el tamaño de la ventana de la consola, después se crea la ventana con la función
+newwin (alto_max, ancho_max, alto_min, ancho_min)
+'''
+s=curses.initscr() #Se inciializa la aplicación de curses.
 curses.curs_set(0)
 sh,sw=s.getmaxyx()
 w=curses.newwin(sh, sw, 0, 0)
 w.keypad(1)
 w.timeout(100)
 
+'''
+Aquí se crea la serpiente, la serpiente será una lista de listas donde se 
+almacenará las coordenadas de cada parte de su cuerpo, snk_x y snk_y son la
+posicion inicial de la serpiente.
+'''
+
 snk_x=sw/4
-snk_Y=sh/2
+snk_y=sh/2
 snake=[
 	[snk_y,snk_x],
 	[snk_y,snk_x-1],
 	[snk_y,snk_x-2]
 ]
 
+'''
+Se crea la comida de la serpiente, en este caso será el simbolo "•", y se posicionara 
+en la ventana
+'''
 food=[sh/2,sw/2]
-w.addch(food[0],food[1],curses.ACS_PI)
+w.addch(food[0],food[1],"•")
 
 key=curses.KEY_RIGHT
 #Definimos la direccion inicial de la serpiente, ya sea arriba, abajo, izquierda o derecha
 
-
-while True:
 #El juego se desarrolla dentro de este while
-
+while True:
     next_key = w.getch()
-#w.getch() espera un caracter, cuando sea introducido se guardara en next_key
+    #w.getch() espera un caracter, cuando sea introducido se guardara en next_key
 
     key = key if next_key == -1 else next_key
     #La llave va a permanecer igual si no se introduce nada, si si se introduce entonces tomara el valor de lo que se introdujo
